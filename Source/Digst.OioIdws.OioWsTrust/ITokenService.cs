fc14eb53823264862224dc1f77a587997f59baf5
mollekas <mollekas@migrateemail.com>
2017-08-09 09:36:10 +0000

    Added missing files.

---
+++ Source/Digst.OioIdws.OioWsTrust/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using System.IdentityModel.Tokens;
+
+namespace Digst.OioIdws.OioWsTrust
+{
+    /// <summary>
+    /// Used for retrieving a token from NemLog-in STS. The token can then be used to call WSP's (Web Service Providers).
+    /// </summary>
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Recieves a token from a STS. The STS endpoint, client certificate and WSP endpointID are configured in the configuration file.
+        /// This method is thread safe.
+        /// </summary>
+        /// <returns>Returns a token.</returns>
+        SecurityToken GetToken();
+    }
+}
---
+++ Source/Digst.OioIdws.OioWsTrust/TokenServiceCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Runtime.Caching;
+using Digst.OioIdws.OioWsTrust.Utils;
+
+namespace Digst.OioIdws.OioWsTrust
+{
+    /// <summary>
+    /// <see cref="ITokenService"/>
+    /// This implementation acts as a proxy to <see cref="TokenService"/> and caches the token from the STS automatically according to the token expiration time.
+    /// </summary>
+    public class TokenServiceCache : ITokenService
+    {
+        private readonly ITokenService _tokenService;
+        private readonly MemoryCache _tokenCache = MemoryCache.Default;
+        private const string TokenKey = "TokenKey";
+        private readonly TimeSpan _cacheClockSkew;
+
+        public TokenServiceCache(TokenServiceConfiguration config)
+        {
+            _tokenService = new TokenService(config);
+            _cacheClockSkew = config.CacheClockSkew;
+        }
+
+        /// <summary>
+        /// <see cref="ITokenService.GetToken()"/>
+        /// Furtermore this implementation caches the token automatically according to the token expiration time.
+        /// </summary>
+        public SecurityToken GetToken()
+        {
+            var securityToken = (SecurityToken)_tokenCache.Get(TokenKey);
+            
+            if (securityToken == null)
+            {
+                 securityToken = _tokenService.GetToken();
+                _tokenCache.Add(new CacheItem(TokenKey, securityToken), new CacheItemPolicy{AbsoluteExpiration = securityToken.ValidTo - _cacheClockSkew });
+            }
+
+            return securityToken;
+        }
+    }
+}
